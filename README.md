620c75ff410ed5405d513a441e3daa533e773213
TakahitoAsakawa <s2022051@stu.musashino-u.ac.jp>
2022-06-05 19:48:20 +0900

    change name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jaresc
+# jamegresc
 目黒区の年度別の資源ごみ回収量・売却益の可視化をしています。
 範囲は2011~2019年の間です。
 y軸は左が回収量(t)、右が売却益(1000円)です。
@@ -8,14 +8,14 @@ The range is between 2011~2019.
 The y-axis is the amount collected (tons) on the left and the profit from sale (1000 yen) on the right.
 
 # Dataset
-・jarescは以下のページからデータをダウンロードして利用しています。\
-・jaresc uses the data downloaded from the following page.
+・jamegrescは以下のページからデータをダウンロードして利用しています。\
+・jamegresc uses the data downloaded from the following page.
 
 https://catalog.data.metro.tokyo.lg.jp/dataset/t131105d0000000018/resource/31bb126f-6f10-433a-9f65-d00986c48385
 
 
-・jarescは、以下の著作物を改変・加工して利用しています。\
-・jaresc uses the following copyrighted materials by modifying and processing them.
+・jamegrescは、以下の著作物を改変・加工して利用しています。\
+・jamegresc uses the following copyrighted materials by modifying and processing them.
 
 著作物のタイトル　資源回収量・収集拠点数・売却益、\
 著作権者　東京都・目黒区、\
--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jaresc", # 名前
+    name="jamegresc", # 名前
     version="0.0.3", # バージョン設定
     author="takahito asakawa", # 名前
     author_email="s2022051@gmail.com", # メ-ルアドレス
     description='A package for visualization of Resource recovery amount and gain on sale data in Meguro', # 説明文書書き換え
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TakahitoAsakawa/jaresc", # GitHubURL
+    url="https://github.com/TakahitoAsakawa/jamegresc", # GitHubURL
     project_urls={
-        "Bug Tracker": "https://github.com/TakahitoAsakawa/jaresc", #GitHubURL
+        "Bug Tracker": "https://github.com/TakahitoAsakawa/jamegresc", #GitHubURL
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,12 +21,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    py_modules=['jaresc'], # 設定するモジュール名
+    py_modules=['jamegresc'], # 設定するモジュール名
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
     entry_points = {
         'console_scripts': [
-            'jaresc = jaresc:main' # src
+            'jamegresc = jamegresc:main' # src
         ]
     },
 )
