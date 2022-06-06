09a20cc59d80b6a621c306227ee4be67812c212c
TakahitoAsakawa <s2022051@stu.musashino-u.ac.jp>
2022-06-07 04:22:30 +0900

    def in all

--- src/jamegresc.py
+++ src/jamegresc.py
@@ -7,23 +7,21 @@ import argparse
 from time import sleep
 import subprocess as sp
 
-parser = argparse.ArgumentParser(prog='jamegresc', description="default：びん")
-parser.add_argument("--resource", "-r", default="びん", 
-choices=["びん", "アルミ缶", "スチール缶", "ペットボトル", "古紙", "プラスチック製容器包装"])
-args = parser.parse_args()
-column = args.resource
+def main():
+    parser = argparse.ArgumentParser(prog='jamegresc', description="default：びん")
+    parser.add_argument("--resource", "-r", default="びん", 
+    choices=["びん", "アルミ缶", "スチール缶", "ペットボトル", "古紙", "プラスチック製容器包装"])
+    args = parser.parse_args()
+    column = args.resource
 
-sleep(20)
-sp.run(["wget", "https://data.bodik.jp/dataset/28417a5e-ec57-4676-9dbf-8c116fba12ce/resource/966c2391-0d93-45ef-b908-e200ac365a9f/download/131105_recyclable_waste.csv"], capture_output=True, text=True, encoding='utf-8').returncode
-data=pd.read_csv("131105_recyclable_waste.csv", encoding="shift-jis")
-data = data.rename(columns={'分別回収_ﾌﾟﾗｽﾁｯｸ製容器包装（ｔ）': '分別回収_プラスチック製容器包装（ｔ）', '分別回収_売薬駅_ﾌﾟﾗｽﾁｯｸ製容器包装（円）': '分別回収_売却益_プラスチック製容器包装（円）'})
-data = data.iloc[:, :14]
-data.fillna(0,inplace=True)
+    sleep(20)
+    sp.run(["wget", "https://data.bodik.jp/dataset/28417a5e-ec57-4676-9dbf-8c116fba12ce/resource/966c2391-0d93-45ef-b908-e200ac365a9f/download/131105_recyclable_waste.csv"], capture_output=True, text=True, encoding='utf-8').returncode
+    data=pd.read_csv("131105_recyclable_waste.csv", encoding="shift-jis")
+    data = data.rename(columns={'分別回収_ﾌﾟﾗｽﾁｯｸ製容器包装（ｔ）': '分別回収_プラスチック製容器包装（ｔ）', '分別回収_売薬駅_ﾌﾟﾗｽﾁｯｸ製容器包装（円）': '分別回収_売却益_プラスチック製容器包装（円）'})
+    data = data.iloc[:, :14]
+    data.fillna(0,inplace=True)
 
-sp.run(["rm", "131105_recyclable_waste.csv"], capture_output=True)
-
-
-def main(column):
+    sp.run(["rm", "131105_recyclable_waste.csv"], capture_output=True)
     t=data['分別回収_拠点数']
     print(t)
     n = len(data[:])
